e16cacb1d6cbaab3719a0deeb812dba5276ca847
zcybupt <zcybupt2016@hotmail.com>
2018-11-15 15:33:09 +0800

    爬取猫眼电影排行

---
+++ CH03/_3.4/maoyan.py
@@ -0,0 +1,41 @@
+import requests
+import re
+
+
+def get_one_page(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/69.0.3497.92 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.text
+    return None
+
+
+def parse_one_page(html):
+    pattern = re.compile(
+        '<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?'
+        'releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>', re.S
+    )
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2].strip(),
+            'actor': item[3].strip()[3:] if len(item[3]) > 3 else '',
+            'time': item[4].strip()[5:] if len(item[4]) > 5 else '',
+            'score': item[5].strip() + item[6].strip()
+        }
+
+
+def main():
+    url = 'http://maoyan.com/board/4'
+    html = get_one_page(url)
+    result = parse_one_page(html)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
